4e57c459ec3c07d7e07a25ecb3a4927e47e8c6c4
johnchronis <johnchronis@di.uoa.gr>
2018-03-25 14:45:30 -0500

    fixed to SV.sh

--- toSV.sh
+++ toSV.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-scp -P 15000 /Users/johnchronis/code/unchangedFromGuthub/MyScheduler/classes/artifacts/MyScheduler_jar/MyScheduler.jar gsmyris@sigmod.imis.athena-innovation.gr:/home/gsmyris/jc2018/
+scp -P 15000 ./classes/artifacts/MyScheduler_jar/MyScheduler.jar gsmyris@sigmod.imis.athena-innovation.gr:/home/gsmyris/
